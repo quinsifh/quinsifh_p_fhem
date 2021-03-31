5f62185427af925a76e401e2a8b0efdf852e4bc7
Siegfried Quinger <sq@online.de>
2021-03-31 09:17:33 +0200

    Update 99_myUtils.pm

--- FHEM/99_myUtils.pm
+++ FHEM/99_myUtils.pm
@@ -823,8 +823,6 @@ my $string = "#P_in_W_chargeStandbyThreshold:				Charging only, when \"P_in_W_ch
 #April-September: -4000;-1500;1500;750;500;90;80;23;18;0;120;60;4;0;1112;PVHH;0;
 #Config created by FHEM";
 
-#update_Fehlerspeicher();
-
 my $timestamp2 = substr(TimeNow(),0,10); #"2020-04-11" 01:00:15  für Logfile Teil1
 my $timestamp3 = substr(TimeNow(),11,8); #2020-04-11 "01:00:15"  für Logfile Teil2
 my $timestamp4 = $timestamp2."_".$timestamp3; #für Logfile Teil1_Teil2
@@ -1099,12 +1097,6 @@ sub prg_Tage_YTD(){
 	}
 
 
-sub myProxyForVentil($){
-my $DEVICE= shift;
- if ($DEVICE =~ m/Ventil_1/) {return "Bewaesserung_Vorgarten_1"}
- if ($DEVICE =~ m/Ventil_2/) {return "Bewaesserung_Vorgarten_aussen"}
- if ($DEVICE =~ m/Ventil_3/) {return "Bewaesserung_Vorgarten_Tropfschlauch"}
-}
 
 #########################################################################
 # do not change below _this_ line.
